856651e4ce7da6bb4625fe0e66e50e266914760f
Acolytes <90561917+ProcessedCarbon@users.noreply.github.com>
2023-09-14 11:59:49 +0800

    Remove commented code

--- PythonFiles/Pages/SchedulePage.py
+++ PythonFiles/Pages/SchedulePage.py
@@ -93,7 +93,6 @@ class SchedulePage(Page):
                 self.UpdatePanel(panel, event)
 
             NERInterface.ClearEvents()
-        #gui.root.after(500, self.Update)  
 
     def CreateICSUsingEntities(self):
 
