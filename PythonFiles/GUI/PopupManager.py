d92209442e5f75d52298db9ff0175014ee16d2ce
Acolytes <90561917+ProcessedCarbon@users.noreply.github.com>
2023-12-16 16:51:44 +0800

    calendar gui styling

--- PythonFiles/GUI/PopupManager.py
+++ PythonFiles/GUI/PopupManager.py
@@ -16,22 +16,22 @@ def PopupWithBtn(subtitle_1:str, subtitle_2:str, button_cb, textbox_content=''):
 
     subtitle_1_label = GUIInterface.CreateLabel(text=subtitle_1, 
                                                 font=GUIInterface.getCTKFont(size=16, weight="bold"))
-    subtitle_1_label.grid(row=0, column=0, sticky='nsew')
+    subtitle_1_label.grid(row=0, column=0, sticky='nsew',pady=10, padx=10)
 
     subtitle_2_label = GUIInterface.CreateLabel(text=subtitle_2, 
                                                 font=GUIInterface.getCTKFont(size=13, weight="normal"))
-    subtitle_2_label.grid(row=1, column=0, sticky='nsew')
+    subtitle_2_label.grid(row=1, column=0, sticky='nsew', pady=10, padx=10)
 
     content = GUIInterface.CreateTextbox()
     GUIInterface.UpdateTextBox(content, 'disabled', textbox_content)
-    if textbox_content != '': content.grid(row=2, column=0, sticky='nsew')
+    if textbox_content != '': content.grid(row=2, column=0, sticky='nsew', pady=10, padx=10)
 
     def onClick():
         button_cb()
         window.destroy()
 
     yes_btn = GUIInterface.CreateButton(on_click=onClick, text='Yes')
-    yes_btn.grid(row=3, column=0, sticky='nsew')
+    yes_btn.grid(row=3, column=0, pady=10)
 
     # Prevent clicking and focus of main window
     window.grab_set()
@@ -51,7 +51,7 @@ def FailedPopup(failed_msg:str):
 
     msg_label = GUIInterface.CreateLabel(text=failed_msg, 
                                         font=GUIInterface.getCTKFont(size=13, weight="bold"))
-    msg_label.grid(row=0, column=0, sticky='nsew')
+    msg_label.grid(row=0, column=0, sticky='nsew', pady=10, padx=10)
 
     # Prevent clicking and focus of main window
     window.grab_set()
@@ -59,7 +59,7 @@ def FailedPopup(failed_msg:str):
 
     GUIInterface.SetCurrentFrame(tmp)
 
-def CreateDateWindow(size='250x250'):
+def CreateDateWindow(size='450x450'):
     tmp = GUIInterface.current_frame
 
     window = GUIInterface.CreateNewWindow(window_name='Choose Date', size=size)
@@ -70,11 +70,32 @@ def CreateDateWindow(size='250x250'):
     window_frame.rowconfigure(0, weight=5)
     window_frame.rowconfigure(1, weight=1)
 
-    cal = Calendar(window_frame, selectmode='day', date_pattern='y-mm-dd')
-    cal.grid(row=0, column=0, sticky='nsew')
+    text_color = GUIInterface.color_palette['CTkButton']['text_color'][0]
+    selected_color = GUIInterface.color_palette['CTkButton']['hover_color'][1]
+    bg = GUIInterface.color_palette['CTkFrame']['fg_color'][0]
+    cal = Calendar(window_frame, selectmode='day', date_pattern='y-mm-dd',
+                   showweeknumbers=False, showothermonthdays=False,
+                   font = GUIInterface.getCTKFont(size=13),
+                   background=GUIInterface.color_palette['CTkButton']['fg_color'][0], # Header buttons
+                   foreground=text_color,
+                   bordercolor=GUIInterface.color_palette['CTkButton']['fg_color'][0],
+                   headersbackground=bg, # header text
+                   headersforeground=text_color,
+                   selectbackground=bg, # selected cell text
+                   selectforeground=selected_color,  
+                   normalbackground=bg, # other week day cells text
+                   normalforeground=text_color,
+                   weekendbackground=bg, # other weekend cells text
+                   weekendforeground=text_color,
+                   othermonthforeground=text_color,
+                   othermonthbackground=bg,
+                   othermonthweforeground=text_color,
+                   othermonthwebackground=bg,
+                   )
+    cal.grid(row=0, column=0, sticky='nsew',pady=10, padx=10)
 
     submit_btn = GUIInterface.CreateButton(on_click=None, text='select')
-    submit_btn.grid(row=1, column=0)
+    submit_btn.grid(row=1, column=0, pady=10, padx=10)
 
     # Prevent clicking and focus of main window
     window.grab_set()
