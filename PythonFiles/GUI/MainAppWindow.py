b19f8c7bacfe23ad9c235e2ed13e916a3a9ffff9
Acolytes <90561917+ProcessedCarbon@users.noreply.github.com>
2023-12-21 11:46:30 +0800

    removed screeninfo

--- PythonFiles/GUI/MainAppWindow.py
+++ PythonFiles/GUI/MainAppWindow.py
@@ -1,35 +1,11 @@
-# from screeninfo import get_monitors
 from GUI.GUIInterface import GUIInterface
 from Calendar.CalendarInterface import CalendarInterface
 import os, signal
-
-# def GetCurrentMonitorInfo()->dict:
-#         # Monitor(x=0, y=0, width=3840, height=2160, width_mm=708, height_mm=399, name='DP-0', is_primary=True)
-#         current = {}
-#         for m in get_monitors():
-#             s = str(m)
-#             # Had to do this way cause splitting with brackets as pattern causes issues
-#             s = s.replace("Monitor","").replace("(", "").replace(")", "").replace(" ","")
-#             split = s.split(",")
-
-#             is_primary_split = split[len(split) - 1].split("=")
-#             is_primary = bool(is_primary_split[1])
-#             if is_primary == False:
-#                 continue
-
-#             for i in range(len(split)):
-#                 var_split = split[i].split("=")
-#                 current[var_split[0]] = var_split[1]
-
-#         return current 
 
 class MainAppWindow:
     aspect = 0.85
     main_frame = None
 
-    # monitor_info = GetCurrentMonitorInfo()
-    # monitor_width = int(monitor_info["width"])
-    # monitor_height = int(monitor_info["height"])
     monitor_width = GUIInterface.monitor_width
     monitor_height =  GUIInterface.monitor_height
     app_width = int(monitor_width * aspect)
